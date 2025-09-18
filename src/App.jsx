6c56230e8b1c75f674a130ac497809025dc401cb
Forerunner78 <alex.ribault@gmail.com>
2025-09-18 18:43:21 +0200

    Search components initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Search from './components/Search'
 
 const App = () => {
+	const [searchTerm, setSearchTerm] = React.useState('');
+
 	return (
-		<h1 class="text-3xl font-bold underline">
-			Hello world!
-		</h1>
+		<main>
+			<div className='pattern' />
+			<div className='wrapper'>
+				<header>
+					<img src="./hero.png" alt="Hero Banner" />
+					<h1>
+						Find <span className='text-gradient'>Movies</span> You'll enjoy without the hassle
+					</h1>
+				</header>
+
+				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+			</div>
+		</main>
 	)
 }
 
